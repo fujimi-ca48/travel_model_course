07ef0e10964c8dbb8d8fcc9d0b42545323e53dbe
fujimi-ca48 <your-ca.fujimi048@gmail.com>
2023-08-13 14:52:31 +0900

    ログイン機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,20 @@
 class UsersController < ApplicationController
   def new
       @user = User.new
-    end
+  end
   
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        redirect_to users_path
-      else
-        render :new
-      end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
     end
+  end
   
-    private
+  private
   
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'top_pages#top'
-  resources :users, only: [:new, :create]
+  resources :users, only: %i[new create]
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 end
