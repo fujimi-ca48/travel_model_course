6a0ed02c883e3100fb9eac9fd95d79085a5bf5bf
fujimi-ca48 <your-ca.fujimi048@gmail.com>
2023-08-14 11:19:19 +0900

    i18n対応

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,15 +5,15 @@ class UserSessionsController < ApplicationController
     
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to root_path, success: 'success'
+      redirect_back_or_to root_path, success: t('.success')
     else
-        flash.now[:danger] = 'ログインに失敗しました'
+        flash.now[:danger] = t('.fail')
         render :new, status: :unprocessable_entity
     end
   end
   
   def destroy
     logout
-    redirect_to root_path, success: 'success'
+    redirect_to root_path, success: t('.success')
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module TestTravelModelCourse
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.available_locales = [:en, :ja]
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
