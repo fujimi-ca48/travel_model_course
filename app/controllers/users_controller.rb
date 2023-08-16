919f48d668af9ab66b3016fc5fcf3bf4214f4bca
fujimi-ca48 <your-ca.fujimi048@gmail.com>
2023-08-16 12:12:27 +0900

    遷移先の変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       auto_login(@user)
-      redirect_to root_path, success: t('.success')
+      redirect_to tourist_spots_path, success: t('.success')
     else
       flash.now[:danger] = t('.fail')
       render :new, status: :unprocessable_entity
