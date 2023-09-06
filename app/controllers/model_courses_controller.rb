64b0062eaa4351caa488db31bde16762c3769b08
fujimi-ca48 <your-ca.fujimi048@gmail.com>
2023-09-06 23:17:20 +0900

    heroku修正

--- app/controllers/model_courses_controller.rb
+++ app/controllers/model_courses_controller.rb
@@ -34,12 +34,10 @@ class ModelCoursesController < ApplicationController
     @model_course = current_user.model_courses.build(model_course_params)
     @model_course.spot_item_data = spot_item_data.to_json  # JSON形式に変換して保存
 
-# transportationの値を集計し、最も多い値をvehicleに設定
-transportation_counts = selected_total_spot_items.group_by { |item| TotalSpotItem.transportations.key(item.transportation) }.transform_values(&:count)
-most_common_transportation_key = transportation_counts.max_by { |_, count| count }&.first
+    transportation_counts = selected_total_spot_items.group_by { |item| TotalSpotItem.transportations.key(item.transportation) }.transform_values(&:count)
+    most_common_transportation_key = transportation_counts.max_by { |_, count| count }&.first
 
-# 最も多いtransportationのキーを使ってvehicleの値を設定
-@model_course.vehicle = most_common_transportation_key if most_common_transportation_key
+    @model_course.vehicle = most_common_transportation_key if most_common_transportation_key
 
   # recommended_spotとtourist_spotのaddressを集計し、最も多い都道府県をprefectureに設定
   address_counts = selected_total_spot_items.map do |item|
