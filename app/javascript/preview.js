b52d85014a1a29d849405b6e3d959b25d4979214
fujimi-ca48 <your-ca.fujimi048@gmail.com>
2023-09-14 22:50:15 +0900

    アップロードを編集画面に追加

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,23 +1,27 @@
-if (document.URL.match(/new/)){
-    document.addEventListener('DOMContentLoaded', () => {
-      const createImageHTML = (blob) => {
-        const imageElement = document.getElementById('new-image');
-        const blobImage = document.createElement('img');
-        blobImage.setAttribute('class', 'new-img')
-        blobImage.setAttribute('src', blob);
+document.addEventListener('turbo:load', () => {
+    const createImageHTML = (blob, previewId) => {
+      const imageElement = document.getElementById(previewId);
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('class', 'new-img');
+      blobImage.setAttribute('src', blob);
   
-        imageElement.appendChild(blobImage);
-      };
+      imageElement.appendChild(blobImage);
+    };
   
-      document.getElementById('img').addEventListener('change', (e) => {
-        const imageContent = document.querySelector('img');
-        if (imageContent){
-          imageContent.remove();
+    const imageInputs = document.querySelectorAll('.image-input');
+  
+    imageInputs.forEach((input) => {
+      input.addEventListener('change', (e) => {
+        const imageContent = e.target.nextElementSibling; // .image-preview element
+        const previewId = imageContent.id;
+        const existingImage = imageContent.querySelector('img');
+        if (existingImage) {
+          existingImage.remove();
         }
   
         const file = e.target.files[0];
         const blob = window.URL.createObjectURL(file);
-        createImageHTML(blob);
+        createImageHTML(blob, previewId);
       });
     });
-  }
+  }); 
